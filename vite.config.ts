eb9453ebb39d923fa9f55dafc5b5689fc75b2c90
Xukai1666666 <2684024189@qq.com>
2023-01-08 14:51:04 +0800

    跨域配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,37 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      // 字符串简写写法：http://localhost:5173/foo -> http://localhost:4567/foo
+      // '/foo': 'http://localhost:4567',
+      // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
+      '/api': {
+        target: 'http://127.0.0.1:8000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      // 正则表达式写法：http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+      // '^/fallback/.*': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/fallback/, ''),
+      // },
+      // 使用 proxy 实例
+      // '/api': {
+      //   target: 'http://127.0.0.1:8000/api',
+      //   changeOrigin: true,
+      //   configure: (proxy, options) => {
+      //     // proxy 是 'http-proxy' 的实例
+      //   }
+      // },
+      // 代理 websockets 或 socket.io 写法：ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+      // '/socket.io': {
+      //   target: 'ws://localhost:5174',
+      //   ws: true,
+      // },
+    },
+  },
 })
+
+
