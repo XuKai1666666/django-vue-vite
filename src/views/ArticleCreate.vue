7b7ec7a0ae725d6fbcc173f8a665dba03286c5c6
Xukai1666666 <2684024189@qq.com>
2023-02-01 21:13:04 +0800

    文章发表页面

---
+++ src/views/ArticleCreate.vue
@@ -0,0 +1,15 @@
+<template>
+    <BlogHeader />
+
+    <BlogFooter />
+</template>
+
+<script lang="ts">
+import BlogHeader from '../components/BlogHeader.vue'
+import BlogFooter from '../components/BlogFooter.vue'
+
+export default {
+    name: 'ArticleCreate',
+    components: { BlogHeader, BlogFooter }
+}
+</script>
